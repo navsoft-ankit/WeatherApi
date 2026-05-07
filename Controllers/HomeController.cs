d78865022b306802cf5c66bc3e3e4c687181340c
Ankit Das <ankit.das@navsoft.in>
2026-05-07 15:33:10 +0530

    Updated weather API logic

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
+using Test.Models;
 namespace Test.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("api/Home")]
     public class HomeController : ControllerBase
     {
         [HttpGet]
         public string Get()
         {
             return"Home Page";
+        }
+        [HttpPost]
+        public ActionResult AddHome([FromBody]Home home)
+        {
+            if(home == null)
+            {
+                return BadRequest("Invlaid");
+            }
+            return Created("api/Home",home);
         }
     }
 }
---
+++ Models.cs/HomeModel.cs
@@ -0,0 +1,8 @@
+namespace Test.Models
+{
+    public class Home
+    {
+        public string Name{get; set;}
+        public int Age{get; set;}
+    }
+}
